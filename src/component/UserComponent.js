88901ba24077e976b9e69503c2105b4e090f5b0d
konepadith <konepadithspydee@gmail.com>
2023-10-02 16:12:16 +0700

    w

--- src/component/UserComponent.js
+++ src/component/UserComponent.js
@@ -4,17 +4,17 @@ function UserComponent(){
     const [user,setUser]=useState([])
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
+        .then(response => response.json() )
         .then(json => setUser(json))
     })
     
     return(
         <>
         {user.map((element)=>{
-            return (<div>
-            <h1 key={element.id}>user id: {element.userId}</h1>
-            <h1 >user title: {element.title}</h1>
-            </div>)
+            return (<ul>
+            <li key={element.id}>user id: {element.userId}  user title: {element.title}</li>
+           
+            </ul>)
         })}
         </>
     )
